be145fcc40839f747ddd76e36976afb08b9de8e6
Althaafzz <althaaf.zaydaan@gmail.com>
2023-01-19 08:33:55 +0700

    belajar if else

---
+++ belajarifelse.php
@@ -0,0 +1,13 @@
+<?php
+
+// metode if else 
+$a = "magic";
+
+// if digunakan untuk kondisi yang diinginkan
+if($a == "magics"){
+    echo "berhasil";
+// else digunakan untuk kondisi tidak sesuai dengan yang diinginkan
+} else {
+    echo "gagal";
+};
+?>
--- programhitungumur.php
+++ programhitungumur.php
@@ -1,7 +1,7 @@
 <?php
 
 $tahun_ini = 2023;
-$tahun_lahir = 2006;
+$tahun_lahir = 2002;
 $umur = $tahun_ini - $tahun_lahir;
 
 echo "Saya lahir pada tahun $tahun_lahir, maka saya berumur $umur tahun";
